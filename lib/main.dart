26b892c4613242693c17c26d658a4c5f73fdaa3d
Guilherme <contato@guilhermemenezes.com>
2023-09-06 17:24:43 +0100

    Update Wiget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(PerguntaApp());
-}
+main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Olá Flutter'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello Flutter!')),
+        body: Text('Olá Flutter!'),
+      ),
     );
   }
 }
